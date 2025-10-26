25c10eb456147c46a957b19588f2768f3fce8b60
Harikrishnan K <132329896+Harikrishnankanjingattu@users.noreply.github.com>
2025-10-26 23:53:49 +0530

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -16,3 +16,26 @@ begin
 select * from courses where course_id=cour; 
 end //
 delimter ;
+
+
+
+DELIMITER //
+
+-- Drop the procedure if it already exists
+DROP PROCEDURE IF EXISTS new //
+
+-- Create a procedure with IN and OUT parameters
+CREATE PROCEDURE new(IN n1 INT, OUT n2 INT)
+BEGIN
+    SELECT salary INTO n2 
+    FROM employee 
+    WHERE emp_id = n1;
+END //
+
+DELIMITER ;
+
+-- Call the procedure
+CALL new(2, @s);
+
+-- Display the OUT parameter result
+SELECT @s AS n2;
